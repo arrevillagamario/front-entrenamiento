011047255bbfaf66dfbdfe1a6a58533d3766f7c1
Kevin Daniel Martínez <kevin.rodriguez.2001m@gmail.com>
2024-09-23 22:57:28 -0600

    Updated login.jsx
    component login updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,43 +1,90 @@
+import React, { useState } from "react";
+import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
-  const handleNavigation = () => {
-    navigate("/home");
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLogin((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3000/user/login", {
+        email: login.email,
+        password: login.password,
+      });
+
+      localStorage.setItem("userData", JSON.stringify(response.data));
+
+      if (response.status === 200) {
+        setSuccessMessage("Login successful!");
+        setErrorMessage("");
+        navigate("/home");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.error) {
+        setErrorMessage(error.response.data.error);
+      } else {
+        setErrorMessage("An error occurred. Please try again.");
+      }
+    }
+  };
+
   return (
-    <div class="bg-gray-900 flex justify-center items-center h-screen">
-      <div class="bg-black bg-opacity-80 p-10 rounded-lg w-96 text-center">
-        <div class="mb-8">
-          <img src="/img/logo.png" alt="Spotify Logo" class="mx-auto w-32" />
+    <div className="bg-gray-900 flex justify-center items-center h-screen">
+      <div className="bg-black bg-opacity-80 p-10 rounded-lg w-96 text-center">
+        <div className="mb-8">
+          <img src="/img/logo.png" alt="Logo" className="mx-auto w-32" />
         </div>
 
-        <div class="flex justify-around text-white mb-6">
-          <Link to={"/"} class="font-bold border-b-2 border-green-500 pb-1">
+        <div className="flex justify-around text-white mb-6">
+          <Link to={"/"} className="font-bold border-b-2 border-green-500 pb-1">
             SIGN IN
           </Link>
-          <Link to={"/registrar"} class="font-bold pb-1">
+          <Link to={"/registrar"} className="font-bold pb-1">
             SIGN UP
           </Link>
         </div>
 
-        <form className="space-y-8">
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        {successMessage && <p className="text-green-500">{successMessage}</p>}
+
+        <form className="space-y-8" onSubmit={handleLogin}>
           <input
-            type="text"
-            placeholder="Username"
-            class="w-full p-3  rounded-full bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500"
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+            value={login.email}
+            className="w-full p-3 rounded-full bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500"
+            required
           />
           <input
             type="password"
+            name="password"
             placeholder="Password"
-            class="w-full p-3  rounded-full bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500"
+            onChange={handleChange}
+            value={login.password}
+            className="w-full p-3 rounded-full bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500"
+            required
           />
 
           <button
-            onClick={handleNavigation}
             type="submit"
-            class="w-full bg-green-500 text-white font-bold py-3 rounded-full hover:bg-green-600 transition duration-300"
+            className="w-full bg-green-500 text-white font-bold py-3 rounded-full hover:bg-green-600 transition duration-300"
           >
             SIGN IN
           </button>
